dfb853f18198fe9ae1fb473a1c0d22afa2289be8
thekristinadawn <thekristinadawn@gmail.com>
2024-02-14 21:05:42 -0600

    remove localhost

---
+++ javascript/index.js
@@ -0,0 +1,5 @@
+let londonElement = document.querySelector("#london");
+let londonDateElement = londonElement.querySelector(".date");
+let londonTimeElement = londonElement.querySelector(".time");
+londonDateElement.innerHTML = "August 15th 2022";
+londonTimeElement.innerHTML = "1:49:15 <small>AM</small>";
